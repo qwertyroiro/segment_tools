364cd479115a66b062368f50b1e03dfa733e46b3
qwertyroiro <roiro8562@gmail.com>
2024-02-09 20:01:52 +0900

    Add submodule for Depth-Anything_segtools and import demo_depth

--- src/segment_tools/__init__.py
+++ src/segment_tools/__init__.py
@@ -13,4 +13,5 @@ if not os.path.exists("weights/sam_vit_h_4b8939.pth"):
 from .demo_seg import *
 from .demo_ground import *
 from .demo_one import *
+from .demo_depth import *
 from .utils import *
---
+++ src/segment_tools/demo_depth.py
@@ -0,0 +1,35 @@
+import sys
+import os
+
+sys.path.append(os.path.join(os.path.dirname(__file__), "Depth-Anything_segtools"))
+from depth_anything.dpt import DepthAnything
+from depth_anything.util.transform import Resize, NormalizeImage, PrepareForNet
+
+import cv2
+import torch
+from torchvision.transforms import Compose
+
+def Depth_Anything(image, encoder='vits'):
+    depth_anything = DepthAnything.from_pretrained('LiheYoung/depth_anything_{:}14'.format(encoder)).eval()
+
+    transform = Compose([
+        Resize(
+            width=518,
+            height=518,
+            resize_target=False,
+            keep_aspect_ratio=True,
+            ensure_multiple_of=14,
+            resize_method='lower_bound',
+            image_interpolation_method=cv2.INTER_CUBIC,
+        ),
+        NormalizeImage(mean=[0.485, 0.456, 0.406], std=[0.229, 0.224, 0.225]),
+        PrepareForNet(),
+    ])
+
+    image = cv2.cvtColor(image, cv2.COLOR_BGR2RGB) / 255.0
+    image = transform({'image': image})['image']
+    image = torch.from_numpy(image).unsqueeze(0)
+
+    # depth shape: 1xHxW
+    depth = depth_anything(image)
+    return depth
