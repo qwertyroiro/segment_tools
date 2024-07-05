2e12be5e4af96c147fc1d645cb2e2c112f112220
qwertyroiro <57554521+qwertyroiro@users.noreply.github.com>
2024-07-05 15:23:50 +0900

    test: add test program

--- all_test.py
+++ all_test.py
@@ -1,8 +1,8 @@
 from PIL import Image
 import numpy as np
 import segment_tools as st
-import cv2
-image_path = "cityscapes.jpg"
+
+image_path = "cityscapes.png"
 image_pil = Image.open(image_path)  # Open image with Pillow
 image_np = np.array(image_pil)      # Convert to numpy array
 
@@ -14,7 +14,7 @@ logging.getLogger("dinov2").setLevel(logging.ERROR)
 
 prompt = "car"  # Define your prompt
 
-
+# fastsam
 fastsam = st.FastSAM()
 result = fastsam.run(image_np)
 if result is not None:
@@ -26,27 +26,31 @@ result = fastsam.run(image_np, prompt)
 if result is not None:
     image, ann = result["image"], result["mask"]
 image.save("fastsam_prompt.jpg")
+del fastsam
 
-
+# clipseg
 clipseg = st.CLIPSeg()
 result = clipseg.run(image_np, prompt)
 if result is not None:
     image, ann = result["image"], result["mask"]
 image.save("clipseg.jpg")
+del clipseg
 
-
+# dino
 dino = st.DINO()
 result = dino.run(image_np, prompt)
 if result is not None:
     image, bbox = result["image"], result["bbox"]
 image.save("dino.jpg")
+del dino
 
-
+# dinoseg
 dinoseg = st.DINOSeg(sam_checkpoint="vit_h")
 result = dinoseg.run(image_np, prompt)
 if result is not None:
     image, ann = result["image"], result["mask"]
 image.save("dinoseg.jpg")
+del dinoseg
 
 # ADE20K
 oneformer_ade20k = st.OneFormer(dataset="ade20k")
@@ -60,6 +64,7 @@ result = oneformer_ade20k.run(image_np, prompt)
 if result is not None:
     image, ann = result["image"], result["mask"]
 image.save("oneformer_ade20k_prompt.jpg")
+del oneformer_ade20k
 
 oneformer_ade20k_swin = st.OneFormer(dataset="ade20k", use_swin=True)
 result = oneformer_ade20k_swin.run(image_np)
@@ -72,9 +77,74 @@ result = oneformer_ade20k_swin.run(image_np, prompt)
 if result is not None:
     image, ann = result["image"], result["mask"]
 image.save("oneformer_ade20k_swin_prompt.jpg")
+del oneformer_ade20k_swin
 
 # Cityscapes
 oneformer_city = st.OneFormer(dataset="cityscapes")
 result = oneformer_city.run(image_np)
 if result is not None:
-image, ann, info = result["image"], result["mask"], result["info"]
+    image, ann, info = result["image"], result["mask"], result["info"]
+image.save("oneformer_city.jpg")
+
+oneformer_city = st.OneFormer(dataset="cityscapes")
+result = oneformer_city.run(image_np, prompt)
+if result is not None:
+    image, ann = result["image"], result["mask"]
+image.save("oneformer_city_prompt.jpg")
+del oneformer_city
+
+oneformer_city_swin = st.OneFormer(dataset="cityscapes", use_swin=True)
+result = oneformer_city_swin.run(image_np)
+if result is not None:
+    image, ann, info = result["image"], result["mask"], result["info"]
+image.save("oneformer_city_swin.jpg")
+
+oneformer_city_swin = st.OneFormer(dataset="cityscapes", use_swin=True)
+result = oneformer_city_swin.run(image_np, prompt)
+if result is not None:
+    image, ann = result["image"], result["mask"]
+image.save("oneformer_city_swin_prompt.jpg")
+del oneformer_city_swin
+
+# COCO
+oneformer_coco = st.OneFormer(dataset="coco")
+result = oneformer_coco.run(image_np)
+if result is not None:
+    image, ann, info = result["image"], result["mask"], result["info"]
+image.save("oneformer_coco.jpg")
+
+oneformer_coco = st.OneFormer(dataset="coco")
+result = oneformer_coco.run(image_np, prompt)
+if result is not None:
+    image, ann = result["image"], result["mask"]
+image.save("oneformer_coco_prompt.jpg")
+del oneformer_coco
+
+oneformer_coco_swin = st.OneFormer(dataset="coco", use_swin=True)
+result = oneformer_coco_swin.run(image_np)
+if result is not None:
+    image, ann, info = result["image"], result["mask"], result["info"]
+image.save("oneformer_coco_swin.jpg")
+
+oneformer_coco_swin = st.OneFormer(dataset="coco", use_swin=True)
+result = oneformer_coco_swin.run(image_np, prompt)
+if result is not None:
+    image, ann = result["image"], result["mask"]
+image.save("oneformer_coco_swin_prompt.jpg")
+del oneformer_coco_swin
+
+# depth
+depth_model = st.Depth_Anything(encoder="vitl") # vits or vitb or vitl
+result = depth_model.run(image)
+if result is not None:
+    image, depth = result["image"], result["depth"]
+image.save("depth.jpg")
+del depth_model
+
+# DINOv2_depth
+depth_model = st.DINOv2_depth(BACKBONE_SIZE="base") # small, base, large, giant
+result = depth_model.run(image)
+if result is not None:
+    depth_img, depth = result["image"], result["depth"]
+depth_img.save("depth_dinov2.jpg")
+del depth_model
