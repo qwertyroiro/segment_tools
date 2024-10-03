b504a27a177a73f23e599aad67ee7b3d92b42cb8
qwertyroiro <57554521+qwertyroiro@users.noreply.github.com>
2024-10-03 18:18:08 +0900

    test: Update OneFormer backbone parameter to use string values

--- all_test.py
+++ all_test.py
@@ -89,26 +89,26 @@ if result is not None:
 save_image(image, "oneformer_ade20k_prompt.jpg")
 del oneformer_ade20k
 
-oneformer_ade20k_swin = st.OneFormer(dataset="ade20k", use_swin=True)
+oneformer_ade20k_swin = st.OneFormer(dataset="ade20k", backbone="swin")
 result = oneformer_ade20k_swin.run(image_np)
 if result is not None:
     image, ann, info = result["image"], result["mask"], result["info"]
 save_image(image, "oneformer_ade20k_swin.jpg")
 
-oneformer_ade20k_swin = st.OneFormer(dataset="ade20k", use_swin=True)
+oneformer_ade20k_swin = st.OneFormer(dataset="ade20k", backbone="swin")
 result = oneformer_ade20k_swin.run(image_np, prompt)
 if result is not None:
     image, ann = result["image"], result["mask"]
 save_image(image, "oneformer_ade20k_swin_prompt.jpg")
 del oneformer_ade20k_swin
 
-oneformer_ade20k_conv = st.OneFormer(dataset="ade20k", use_convnext=True)
+oneformer_ade20k_conv = st.OneFormer(dataset="ade20k", backbone="convnext")
 result = oneformer_ade20k_conv.run(image_np)
 if result is not None:
     image, ann, info = result["image"], result["mask"], result["info"]
 save_image(image, "oneformer_ade20k_convnext.jpg")
 
-oneformer_ade20k_conv = st.OneFormer(dataset="ade20k", use_convnext=True)
+oneformer_ade20k_conv = st.OneFormer(dataset="ade20k", backbone="convnext")
 result = oneformer_ade20k_conv.run(image_np, prompt)
 if result is not None:
     image, ann = result["image"], result["mask"]
@@ -130,26 +130,26 @@ if result is not None:
 save_image(image, "oneformer_city_prompt.jpg")
 del oneformer_city
 
-oneformer_city_swin = st.OneFormer(dataset="cityscapes", use_swin=True)
+oneformer_city_swin = st.OneFormer(dataset="cityscapes", backbone="swin")
 result = oneformer_city_swin.run(image_np)
 if result is not None:
     image, ann, info = result["image"], result["mask"], result["info"]
 save_image(image, "oneformer_city_swin.jpg")
 
-oneformer_city_swin = st.OneFormer(dataset="cityscapes", use_swin=True)
+oneformer_city_swin = st.OneFormer(dataset="cityscapes", backbone="swin")
 result = oneformer_city_swin.run(image_np, prompt)
 if result is not None:
     image, ann = result["image"], result["mask"]
 save_image(image, "oneformer_city_swin_prompt.jpg")
 del oneformer_city_swin
 
-oneformer_city_conv = st.OneFormer(dataset="cityscapes", use_convnext=True)
+oneformer_city_conv = st.OneFormer(dataset="cityscapes", backbone="convnext")
 result = oneformer_city_conv.run(image_np)
 if result is not None:
     image, ann, info = result["image"], result["mask"], result["info"]
 save_image(image, "oneformer_city_convnext.jpg")
 
-oneformer_city_conv = st.OneFormer(dataset="cityscapes", use_convnext=True)
+oneformer_city_conv = st.OneFormer(dataset="cityscapes", backbone="convnext")
 result = oneformer_city_conv.run(image_np, prompt)
 if result is not None:
     image, ann = result["image"], result["mask"]
@@ -171,13 +171,13 @@ if result is not None:
 save_image(image, "oneformer_coco_prompt.jpg")
 del oneformer_coco
 
-oneformer_coco_swin = st.OneFormer(dataset="coco", use_swin=True)
+oneformer_coco_swin = st.OneFormer(dataset="coco", backbone="swin")
 result = oneformer_coco_swin.run(image_np)
 if result is not None:
     image, ann, info = result["image"], result["mask"], result["info"]
 save_image(image, "oneformer_coco_swin.jpg")
 
-oneformer_coco_swin = st.OneFormer(dataset="coco", use_swin=True)
+oneformer_coco_swin = st.OneFormer(dataset="coco", backbone="swin")
 result = oneformer_coco_swin.run(image_np, prompt)
 if result is not None:
     image, ann = result["image"], result["mask"]
@@ -200,26 +200,26 @@ if result is not None:
 save_image(image, "oneformer_vistas_prompt.jpg")
 del oneformer_vistas
 
-oneformer_vistas_swin = st.OneFormer(dataset="vistas", use_swin=True)
+oneformer_vistas_swin = st.OneFormer(dataset="vistas", backbone="swin")
 result = oneformer_vistas_swin.run(image_np)
 if result is not None:
     image, ann, info = result["image"], result["mask"], result["info"]
 save_image(image, "oneformer_vistas_swin.jpg")
 
-oneformer_vistas_swin = st.OneFormer(dataset="vistas", use_swin=True)
+oneformer_vistas_swin = st.OneFormer(dataset="vistas", backbone="swin")
 result = oneformer_vistas_swin.run(image_np, prompt)
 if result is not None:
     image, ann = result["image"], result["mask"]
 save_image(image, "oneformer_vistas_swin_prompt.jpg")
 del oneformer_vistas_swin
 
-oneformer_vistas_conv = st.OneFormer(dataset="vistas", use_convnext=True)
+oneformer_vistas_conv = st.OneFormer(dataset="vistas", backbone="convnext")
 result = oneformer_vistas_conv.run(image_np)
 if result is not None:
     image, ann, info = result["image"], result["mask"], result["info"]
 save_image(image, "oneformer_vistas_convnext.jpg")
 
-oneformer_vistas_conv = st.OneFormer(dataset="vistas", use_convnext=True)
+oneformer_vistas_conv = st.OneFormer(dataset="vistas", backbone="convnext")
 result = oneformer_vistas_conv.run(image_np, prompt)
 if result is not None:
     image, ann = result["image"], result["mask"]
