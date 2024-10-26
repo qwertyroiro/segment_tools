2327a038868b0ec195f0d8fe8d2586821a5c8533
qwertyroiro <57554521+qwertyroiro@users.noreply.github.com>
2024-10-26 16:55:41 +0900

    refact: Rename InsightfaceModule to Insightface

--- src/segment_tools/insightface_module.py
+++ src/segment_tools/insightface_module.py
@@ -4,7 +4,7 @@ import torch
 from .utils import check_image_type
 
 
-class InsightfaceModule:
+class Insightface:
     def __init__(self):
         self.app = insightface.app.FaceAnalysis()
         device = torch.device("cuda" if torch.cuda.is_available() else "cpu")
