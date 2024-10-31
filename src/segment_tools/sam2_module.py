d47d5539b952a1162055e7ecf181cc2971f2aa01
qwertyroiro <57554521+qwertyroiro@users.noreply.github.com>
2024-10-31 17:40:25 +0900

    fix: fourcc codec to 'avc1' in SAM2 module

--- src/segment_tools/sam2_module.py
+++ src/segment_tools/sam2_module.py
@@ -133,7 +133,7 @@ class SAM2:
         fps = int(cap.get(cv2.CAP_PROP_FPS))
         width = int(cap.get(cv2.CAP_PROP_FRAME_WIDTH))
         height = int(cap.get(cv2.CAP_PROP_FRAME_HEIGHT))
-        fourcc = cv2.VideoWriter_fourcc(*'mp4v')
+        fourcc = cv2.VideoWriter_fourcc(*'avc1')
         
         if os.path.exists("temp_output"):
             shutil.rmtree("temp_output")
